dfd398508a46617ed22e67e16d6b8810d341d1c5
Moustafa Elhadary <moustafa.elhadary@logisticsplus.net>
2019-07-10 21:16:17 -0400

    author update

--- src/Article.js
+++ src/Article.js
@@ -7,16 +7,15 @@ const Article = ({ title, author, source, image, content, url }) => {
 
     return (
 
-        <Card style={{ width: '30rem' }}>
+        <Card style={{ width: '30rem', margin: '2em' }}>
+        <Card.Header>{title}</Card.Header>
             <Card.Img variant="top" src={image} alt="" width="300vh" />
             <Card.Body>
-                <Card.Title>{title}</Card.Title>
                 <Card.Title>{author}</Card.Title>
                 <Card.Title>{source}</Card.Title>
-
                 <Card.Text>{content}</Card.Text>
-                <a href={url} target="_blank" rel="noopener noreferrer"> go to article</a>
-                <Button variant="primary">Go somewhere</Button>
+                
+                <Button variant="primary"><a href={url} target="_blank" rel="noopener noreferrer" style={{ textDecoration: 'none', color: 'white' }}> go to article</a></Button>
             </Card.Body>
 
 
